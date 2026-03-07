2229351720efde38c2b04eac00af18b484bc5c2e
Oshaq Naimat <oshaqnaimat15@gmail.com>
2026-03-07 20:36:27 +0500

    navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -3,19 +3,27 @@
     <i class="bi bi-list text-3xl"></i>
     <img class="w-[70px] " src="https://thumbs.dreamstime.com/b/neon-youtube-icon-beautiful-glowing-led-light-157850307.jpg" alt="">
 </div>
-<div class="search flex gap-2">
+<div class="search flex gap-2 items-center justify-center">
     <div class="flex rounded-full bg-secondary items-center justify-center">
     <x-searchbar/>
-    </div>
+</div>
+<div class="hover:bg-red-500 transition p-2 rounded-full cursor-pointer">
+    <i class="bi bi-mic"></i>
+
+</div>
 
 </div>
 <div class="account flex gap-1 items-center justify-center">
-<div class="flex bg-secondary items-center p-2 rounded-full gap-1 justify-center">
+<div class="flex bg-secondary items-center p-2 rounded-full gap-1 justify-center cursor-pointer hover:bg-gray-100">
     <i class="bi bi-plus text-xl"></i>
     <span>Create</span>
 </div>
 <div class="hover:bg-red-500 transition p-2 rounded-full cursor-pointer">
     <i class="bi bi-bell"></i>
 </div>
+<div class="hover:bg-red-500 transition p-2 rounded-full cursor-pointer">
+    <i class="bi bi-person"></i>
+</div>
+
 </div>
 </div>
