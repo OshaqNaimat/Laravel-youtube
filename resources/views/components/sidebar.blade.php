312ad6cee15d35c247cc38fc7a72dcfce055ca35
Oshaq Naimat <oshaqnaimat15@gmail.com>
2026-03-07 23:06:17 +0500

    sidebar completed

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,5 +1,11 @@
+<style>
+    .sidebarscroll::-webkit-scrollbar{
+        display: none
+    }
+</style>
+<h1>text to be shown</h1>
 
-<div class="contaienr sticky left-0 top-10 border-red-500 border w-1/6 ">
+<div class="contaienr bg-black fixed left-0 top-17 sidebarscroll  w-1/6 overflow-y-scroll h-[90vh]">
 <div class="flex text-white my-2 text-lg gap-2 p-2 hover:bg-red-500 cursor-pointer rounded-md transition">
     <i class="bi bi-house"></i>
     <span>Home</span>
@@ -34,5 +40,40 @@
    <span>Channel Name</span>
 </div>
 
-
+<hr  class="h-px text-gray-500">
+<div class="flex text-white text-lg my-2 gap-2 p-2 hover:bg-red-500 cursor-pointer rounded-md transition">
+    <span>You</span>
+<i class="bi bi-arrow-right"></i>
 </div>
+<div class="flex text-white text-lg my-2 gap-2 p-2 hover:bg-red-500 cursor-pointer rounded-md transition">
+<i class="bi bi-clock"></i>
+  <span>History</span>
+</div>
+<div class="flex text-white text-lg my-2 gap-2 p-2 hover:bg-red-500 cursor-pointer rounded-md transition">
+<i class="bi bi-list"></i>
+  <span>Playlists</span>
+</div>
+<div class="flex text-white text-lg my-2 gap-2 p-2 hover:bg-red-500 cursor-pointer rounded-md transition">
+<i class="bi bi-clock-history"></i>
+  <span>Watch Later</span>
+</div>
+<div class="flex text-white text-lg my-2 gap-2 p-2 hover:bg-red-500 cursor-pointer rounded-md transition">
+<i class="bi bi-hand-thumbs-up"></i>
+  <span>Liked Videos</span>
+</div>
+<div class="flex text-white text-lg my-2 gap-2 p-2 hover:bg-red-500 cursor-pointer rounded-md transition">
+<i class="bi bi-play"></i>
+  <span>Your Videos</span>
+</div>
+<div class="flex text-white text-lg my-2 gap-2 p-2 hover:bg-red-500 cursor-pointer rounded-md transition">
+<i class="bi bi-download"></i>
+  <span>Download</span>
+</div>
+<hr  class="h-px text-gray-500">
+<div class="flex text-white text-lg my-2 mb-5 gap-2 p-2 hover:bg-red-500 cursor-pointer rounded-md transition">
+<i class="bi bi-box-arrow-left"></i>
+  <span>Logout</span>
+</div>
+</div>
+
+
